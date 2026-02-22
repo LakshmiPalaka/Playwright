10bb4618ad85285a12a5d84528345c325ceb26e9
Palaka Lakshmi Bharani <94096810+LakshmiPalaka@users.noreply.github.com>
2026-02-23 03:12:51 +0530

    Primitive&NonPrimitive Datatypes
    Primitive are immutable & Non Primitive are mutable

---
+++ NonPrimitiveDatatypes.js
@@ -0,0 +1,57 @@
+//Array datatype will be displayed as "object"
+
+Array1 = [1, "lakshmi", true, undefined, null]
+console.log(typeof(Array1))
+
+z = "....................."
+console.log(z)
+
+// Identify individual datatypes of an array
+console.log(typeof(Array1[0]))
+console.log(Array1[0])
+console.log(z)
+console.log(typeof(Array1[1]))
+console.log(Array1[1])
+console.log(z)
+console.log(typeof(Array1[2]))
+console.log(Array1[2])
+console.log(z)
+console.log(typeof(Array1[3]))
+console.log(Array1[3])
+console.log(z)
+console.log(typeof(Array1[4]))
+console.log(Array1[4])
+console.log(z)
+
+// Objects _ an objects properties can be accessed.
+
+makeup = {
+
+    Lipstick : "pink",
+    Eyebrow : "Black",
+    EyelashSize : 18,
+    orderstatus : true,
+    Eventdate : undefined,
+    Membersattending : null
+
+}
+
+console.log(makeup.Eventdate)
+console.log(typeof(makeup.Eventdate))
+console.log(z)
+console.log(makeup.Eyebrow)
+console.log(typeof(makeup.Eyebrow))
+console.log(z)
+console.log(makeup.EyelashSize)
+console.log(typeof(makeup.EyelashSize))
+console.log(z)
+console.log(makeup.Lipstick)
+console.log(typeof(makeup.Lipstick))
+console.log(z)
+console.log(makeup.Membersattending)
+console.log(typeof(makeup.Membersattending))
+console.log(z)
+console.log(makeup.orderstatus)
+console.log(typeof(makeup.orderstatus))
+console.log(z)
+
