b87c821200d03c4724710d3c00168d9ae3db2cb5
Palaka Lakshmi Bharani <94096810+LakshmiPalaka@users.noreply.github.com>
2026-02-23 02:03:05 +0530

    login using variable const

---
+++ letVarLogin.spec.js
@@ -0,0 +1,19 @@
+import { test, expect } from '@playwright/test';
+
+
+test('Login with valid credentials', async ({ page }) => {
+  
+const username = "Admin";
+
+const password = "admin123";
+
+  await page.goto('/web/index.php/auth/login');
+  await page.getByRole('textbox', { name: 'Username' }).click();
+  await page.getByRole('textbox', { name: 'Username' }).fill(username);
+  await page.getByRole('textbox', { name: 'Password' }).click();
+  await page.getByRole('textbox', { name: 'Password' }).fill(password);
+  await page.waitForTimeout(3000);
+  await page.getByRole('button', { name: 'Login' }).click();
+  await expect(page.getByRole('heading', { name: 'Dashboard' })).toBeVisible();
+  await page.close();
+});
