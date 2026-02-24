67fc0346a8d65f4b224f0ab72d0b6f71e0329885
Palaka Lakshmi Bharani <94096810+LakshmiPalaka@users.noreply.github.com>
2026-02-25 01:49:47 +0530

    Conditional Statements

---
+++ IfelseCondition.js
@@ -0,0 +1,10 @@
+if(false){
+    console.log("Palaka")
+    console.log("Lakshmi")
+    console.log("Bharani")
+}
+else {
+    console.log("Nagireddy")
+    console.log("Jagadish")
+    console.log("Khashvi")
+}
