a7dbc07423c311002d328a6968d73975795e4026
Palaka Lakshmi Bharani <94096810+LakshmiPalaka@users.noreply.github.com>
2026-02-25 23:31:13 +0530

    SwitchCase Conditional Statements

---
+++ switchcases.js
@@ -0,0 +1,22 @@
+switch ("automation testing") {
+    case "manual testing": {
+        console.log("Manual testing is being performed.")
+    }
+        break;
+    case "automation testing": {
+        console.log("Automation testing is being performed.")
+    }
+        break;
+    case "performance testing": {
+        console.log("Performance testing is being performed.")
+    }
+        break;
+    case "security testing": {
+        console.log("Security testing is being performed.")
+    }
+        break;
+    default: {
+        console.log("please select a valid testing type.")
+    }
+        break;
+}
