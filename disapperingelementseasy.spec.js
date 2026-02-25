c139b917336d1139981a12f9cef0c866ef78933f
Palaka Lakshmi Bharani <94096810+LakshmiPalaka@users.noreply.github.com>
2026-02-25 23:42:58 +0530

    Ifelse only

--- disapperingelementseasy.spec.js
+++ disapperingelementseasy.spec.js
@@ -4,27 +4,18 @@ import { test, expect } from '@playwright/test';
 test('Disappering elements', async ({ page }) => {
     await page.goto('https://the-internet.herokuapp.com/disappearing_elements');
 
-    const buttons = page.locator('//div[@class="example"]/ul/li/a')
-    const count = await buttons.count();
-
-    if (count == 4) {
-        console.log('4 buttons are visible, clicking on portfolio');
-        await page.locator("//a[text()='Portfolio']").click();
-        await page.waitForTimeout(2000);
+    if(await page.locator('//li/a[text()="Gallery"]').isVisible())
+    {
+        await page.locator('//li/a[text()="Gallery"]').click();
+        console.log("clicked on Gallery");
     }
 
-    else if (count == 5) {
-        console.log('5 buttons are visible, clicking on gallery');
-        await page.locator("//a[text()='Gallery']").click();
-        await page.waitForTimeout(2000);
+    else 
+    {
+        await page.locator('//li/a[text()="Portfolio"]').click();
+        console.log("clicked on Portfolio");
     }
-    else {
-        console.log('refreshing again');
-        await page.reload();
-    }
-    await page.locator("//h1[text()='Not Found']").isVisible();
-    await page.goBack();
-    await page.waitForTimeout(2000);
+    await page.waitForTimeout(2000);    
     await page.close();
 }
 )
