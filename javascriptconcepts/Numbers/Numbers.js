005ac6a6b56a7aea85394311e4792d5093b61426
Lakshmi Palaka <lakshmi.palaka27@gmail.com>
2026-03-13 14:39:08 +0530

    Added Playwright files

---
+++ javascriptconcepts/Numbers/Numbers.js
@@ -0,0 +1,78 @@
+
+
+n1 =5
+n2 = 10.45
+
+//Big Integers - any number which is 2^53 is a big integer.
+num1 = 678903434643562535672325235346647
+num2 = 5
+
+console.log(num1+num2) // output = 6.789034346435626e+32
+
+num3 = 678903434643562535672325235346647n
+num4 = 5n
+
+console.log(num3+num4) // output = 678903434643562535672325235346652n
+
+// conversion of string to number
+
+str = Number ("10")
+console.log(str)
+
+// conversion of string to number
+
+num = String (10)
+console.log(num)
+
+// ParseInt & ParseFloat
+str = parseInt(12.8)
+str = parseFloat("12.8")
+
+// x = 34 ="Raju"
+// console.log(isNan(x))
+
+maxnum = Math.max(78, 21, 2, 323, 534, 1, 4634, 6222, 112, 1)
+console.log(maxnum)
+
+minnum = Math.min(78, 21, 2, 323, 534, 1, 4634, 6222, 112, 1)
+console.log(minnum)
+
+x = 45.67
+y = Math.trunc(x)
+console.log(y)
+
+x = 6.54
+y = Math.round(x)
+console.log(y)
+
+numfloor = 6.1
+result = Math.floor(numfloor)
+console.log(result)
+
+numceil = 6.1
+result = Math.ceil(numceil)
+console.log(result)
+
+numpow = Math.pow(2, 6)
+console.log(numpow)
+
+numrandom = Math.random()
+console.log(numrandom)
+
+numabs = Math.abs()
+console.log(Math.abs(-15))
+
+console.log(Math.sqrt(16));
+
+
+
+
+
+
+
+
+
+
+
+
+
